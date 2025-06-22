52bb2e0c4c631c86ce0d14911ffc396c230ff3d6
Bart Massey <bart.massey@gmail.com>
2025-06-22 16:41:37 -0700

    got ultrasound square wave working

---
+++ src/bin/us.rs
@@ -0,0 +1,50 @@
+#![no_std]
+#![no_main]
+
+use panic_rtt_target as _;
+
+use cortex_m_rt::entry;
+use embedded_hal::delay::DelayNs;
+use num_traits::float::Float;
+use microbit::{Board, hal::{gpio, pwm, time, timer}};
+
+#[entry]
+fn main() -> ! {
+    let board = Board::take().unwrap();
+
+    #[cfg(feature="ext")]
+    let speaker_pin = board.edge.e00;
+    #[cfg(not(feature="ext"))]
+    let speaker_pin = board.speaker_pin;
+
+    let speaker_pin = speaker_pin
+        .into_push_pull_output(gpio::Level::Low)
+        .degrade();
+    let mut timer = timer::Timer::new(board.TIMER0);
+    let pwm = pwm::Pwm::new(board.PWM0);
+
+    let div = pwm::Prescaler::Div1;
+    let counter_mode = pwm::CounterMode::UpAndDown;
+    pwm
+        .set_output_pin(pwm::Channel::C0, speaker_pin)
+        .set_prescaler(div)
+        .set_counter_mode(counter_mode)
+        .set_period(time::Hertz(20_000));
+
+    let mut high = true;
+    let duty = pwm.max_duty() as f32;
+    let width_high = (duty * 0.1).floor() as u16;
+    let width_low = (duty * 0.9).floor() as u16;
+    loop {
+        let width = if high {
+            width_high
+        } else {
+            width_low
+        };
+        high = !high;
+        pwm.set_duty_off_common(width);
+
+        // 440Hz = 1e6 / 440 / 2 = 1136 us, but subtract about 30us for overhead.
+        timer.delay_us(1106);
+    }
+}
